6fbaa5357c1ef9ac1c2ef64076e47f60863e6971
Juan Almanza <hi@scidroid.co>
2022-05-26 18:40:08 -0500

    feat: :sparkles: Add print mode

--- src/components/Notice/index.jsx
+++ src/components/Notice/index.jsx
@@ -3,7 +3,7 @@ import styles from "./notice.module.css";
 
 const Notice = ({ children }) => {
   return (
-    <article className={styles.notice}>
+    <article className={styles.notice + " notice"}>
       <p>{children}</p>
     </article>
   );
